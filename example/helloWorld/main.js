3192a96564e576e5b6e1e234faf53e0a64999094
bingkun.yan <bingkun.yan@7road.com>
2021-06-04 10:00:35 +0800

    mini

--- example/helloWorld/main.js
+++ example/helloWorld/main.js
@@ -1,5 +1,5 @@
 import { createApp } from "../../lib/mini-vue.esm.js";
 import App from "./App.js";
-
+console.log(App)
 const rootContainer = document.querySelector("#root");
 createApp(App).mount(rootContainer);
